24ab3ccaa85b6928882da80c29ec6c45d07ce2a3
XhmikosR <xhmikosr@gmail.com>
2019-04-13 15:43:33 +0300

    Revert "Revert "Update jQuery to v3.4.0.""

--- js/tests/karma.conf.js
+++ js/tests/karma.conf.js
@@ -8,7 +8,7 @@ const {
   browsersKeys
 } = require('./browsers')
 
-const jqueryFile = process.env.USE_OLD_JQUERY ? 'https://code.jquery.com/jquery-1.9.1.min.js' : 'node_modules/jquery/dist/jquery.slim.min.js'
+const jqueryFile = process.env.USE_OLD_JQUERY ? 'https://code.jquery.com/jquery-1.9.1.js' : 'node_modules/jquery/dist/jquery.slim.js'
 const bundle = process.env.BUNDLE === 'true'
 const browserStack = process.env.BROWSER === 'true'
 
@@ -92,7 +92,7 @@ if (bundle) {
   conf.browsers = browsersKeys
   reporters.push('BrowserStack')
   files = files.concat([
-    'node_modules/jquery/dist/jquery.slim.min.js',
+    'node_modules/jquery/dist/jquery.slim.js',
     'js/dist/util.js',
     'js/dist/tooltip.js',
     'js/dist/!(util|index|tooltip).js' // include all of our js/dist files except util.js, index.js and tooltip.js
